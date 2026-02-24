cabcd8a3fb7c28a3f50b098d00c6cf573ddfb65d
Omkar Sutar <omkarsutar37@gmail.com>
2026-02-24 22:21:28 +0530

    after

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4012136925"
+    serviceWorkerVersion: "1966256388"
   }
 });

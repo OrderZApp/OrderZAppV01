7c9da111d2344137d4fec46266eaa1088e63c4bd
Omkar Sutar <omkarsutar37@gmail.com>
2026-02-25 23:26:55 +0530

    agian

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ccc92b67570ba5354cccd5ff92f51243
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "0a05ebbd3d75901db887ddc850febaa4",
+"flutter_bootstrap.js": "1335e4b40363891dac5ea7ab6e8e3244",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
